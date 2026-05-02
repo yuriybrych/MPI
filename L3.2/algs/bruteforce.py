8ff6357a687250f1d6bebbfb5ba3ec5109f91aa2
yuriybrych <brych.yurii@student.uzhnu.edu.ua>
2026-05-02 15:27:35 +0300

    [BL_BF_ALG] | BruteForce algorithm added

---
+++ L3.2/algs/bruteforce.py
@@ -0,0 +1,29 @@
+import itertools
+
+# дозволю собі повийожуватися новим синтаксисом :>
+def BruteForce(
+    capacity: int,
+    weights: list[int],
+    values: list[int]
+) -> tuple[
+    int,
+    list[int]
+]:
+    #
+    #   Повний перебір
+    #   Повертається кортеж: (макс. цінність, список індексів вибраних предметів)
+    #
+    n = len(weights)
+    bestValue = 0
+    bestCombination = []
+
+    for r in range(1, n + 1):
+        for combo in itertools.combinations(range(n), r):
+            currentWeight = sum(weights[i] for i in combo)
+            currentvalue = sum(values[i] for i in combo)
+
+            if currentWeight <= capacity and currentvalue > bestValue:
+                bestValue = currentvalue
+                bestCombination = list(combo)
+
+    return bestValue, bestCombination
